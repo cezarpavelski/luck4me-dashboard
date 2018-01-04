285c1aecf9e34c4dbf8323fd9d321db453c62db1
Cezar Aluisio Pavelski <cezarpavelski@gmail.com>
2018-01-03 23:44:00 -0200

    Refactored offer list  in offers page

--- resources/views/offers/main.blade.php
+++ resources/views/offers/main.blade.php
@@ -44,7 +44,7 @@
                                         <th>Título</th>
                                         <th>Descrição</th>
                                         <th>Desconto</th>
-                                        <th>Tipo</th>
+                                        <th>Destaque</th>
                                         <th>Data de Início</th>
                                     </tr>
                                     </thead>
@@ -53,42 +53,42 @@
                                         <td>Tiger Nixon</td>
                                         <td>System Architect</td>
                                         <td>Edinburgh</td>
-                                        <td>61</td>
+                                        <td>Sim</td>
                                         <td>2011/04/25</td>
                                     </tr>
                                     <tr>
                                         <td>Garrett Winters</td>
                                         <td>Accountant</td>
                                         <td>Tokyo</td>
-                                        <td>63</td>
+                                        <td>Não</td>
                                         <td>2011/07/25</td>
                                     </tr>
                                     <tr>
                                         <td>Ashton Cox</td>
                                         <td>Junior Technical Author</td>
                                         <td>San Francisco</td>
-                                        <td>66</td>
+                                        <td>Não</td>
                                         <td>2009/01/12</td>
                                     </tr>
                                     <tr>
                                         <td>Cedric Kelly</td>
                                         <td>Senior Javascript Developer</td>
                                         <td>Edinburgh</td>
-                                        <td>22</td>
+                                        <td>Não</td>
                                         <td>2012/03/29</td>
                                     </tr>
                                     <tr>
                                         <td>Airi Satou</td>
                                         <td>Accountant</td>
                                         <td>Tokyo</td>
-                                        <td>33</td>
+                                        <td>Não</td>
                                         <td>2008/11/28</td>
                                     </tr>
                                     <tr>
                                         <td>Brielle Williamson</td>
                                         <td>Integration Specialist</td>
                                         <td>New York</td>
-                                        <td>61</td>
+                                        <td>Não</td>
                                         <td>2012/12/02</td>
                                     </tr>
                                     </tbody>
